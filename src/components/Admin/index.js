3670f3ed9bc7505bdeba8451a2b434ce68e9a45f
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2020-12-21 01:31:10 +0200

    Deleted unnecessary files

--- src/components/Admin/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AdminPage = () => (
-  <div>
-    <h1>Admin Page</h1>
-  </div>
-);
-
-export default AdminPage;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,13 +6,6 @@ import {
 } from 'react-router-dom';
 
 import Navigation from '../Navigation';
-import LandingPage from '../Landing';
-import SignUpPage from '../SignUp';
-import SignInPage from '../SignIn';
-import PasswordForgetPage from '../PasswordForget';
-import HomePage from '../Home';
-import AccountPage from '../Account';
-import AdminPage from '../Admin';
 import PostListPage from '../PostList';
 import PostPage from '../Post';
 
