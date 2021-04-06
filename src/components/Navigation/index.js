c8eec3337c9f9e41c6634aebbb5752d499c80b44
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2021-04-06 14:21:02 +0200

    Fixed the navbar, header, and footer

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -11,28 +11,24 @@ import logo from './img/logo.png';
 class Navigation extends React.Component {
   render() {
     return (
-      <div id='navbar'>
-        <div class='container'>
-          <div id="navbar-landing">
+      <div id='navbar' class='container'>
+        <ul>
+          <li id='navbar-title' style={{'float': 'left'}}>
             <a href={ROUTES.LANDING}>
               <img src={logo} alt='' />
-              <div id='navbar-landing-title'><p>Oleksandr's Blog</p></div>
+              <p>Oleksandr's Blog</p>
             </a>
-          </div>
-          <div id='navbar-navigation' class='tc'>
-            <ul>
-              <li>
-                <Link to={ROUTES.LANDING}>About</Link>
-              </li>
-              <li>
-                <Link to={ROUTES.LANDING}>Articles</Link>
-              </li>
-              <li>
-                <Link to={'drafts'}>Categories</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+          </li>
+          <li>
+            <Link to={ROUTES.LANDING}>About</Link>
+          </li>
+          <li>
+            <Link to={ROUTES.LANDING}>Articles</Link>
+          </li>
+          <li>
+            <Link to={'drafts'}>Categories</Link>
+          </li>
+        </ul>
       </div>
     );
   }
